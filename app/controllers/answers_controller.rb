de811f5621236e9c5f535fefe50a76761a867dd1
David Ackerson <david.ackerson2@gmail.com>
2014-06-17 22:50:43 -0300

    After creating new answer, redirects to questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -28,7 +28,7 @@ class AnswersController < ApplicationController
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.html { redirect_to '/questions', notice: 'Answer was successfully created.' }
         format.json { render action: 'show', status: :created, location: @answer }
       else
         format.html { render action: 'new' }
