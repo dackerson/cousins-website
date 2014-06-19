0236180fd3e54b83d4e9f5097bfc4b64e34e3080
David Ackerson <david.ackerson2@gmail.com>
2014-06-18 21:26:15 -0300

    Added Basic Auth

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
   # GET /answers
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate
+    authenticate_or_request_with_http_basic('asdf') do |user, pass|
+      puts "qwer"
+      pass_sha1 = Digest::SHA1.hexdigest(pass)
+      user == 'admin' && pass_sha1 == 'd033e22ae348aeb5660fc2140aec35850c4da997'
+    end
+  end
 end
