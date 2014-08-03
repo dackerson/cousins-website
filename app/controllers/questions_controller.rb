c384976c6d0e36df78d730d5a4c817930b7157bd
David Ackerson <david.ackerson2@gmail.com>
2014-08-02 21:38:16 -0300

    After adding a question, it will redirect to questinos

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -29,8 +29,8 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @question }
+        format.html { redirect_to '/questions', notice: 'Question was successfully created.' }
+        format.json { render action: 'questions#index', status: :created, location: @question }
       else
         format.html { render action: 'new' }
         format.json { render json: @question.errors, status: :unprocessable_entity }
