bcee9128c82465074892f027630146dd4db5cf7c
lucasboko <lucasboko@users.noreply.github.com>
2014-08-19 10:47:07 -0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="x-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width" />
+
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/cousinwebsiteadmin.css" />
+
+        <script src="js/jquery-1.10.2.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/html5"></script>
+        <script src="js/cousinwebsite.js"></script>        
+
+    </head>
+    <body>
+        <div class="container"> 
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4" style="text-align: center;">
+                    <h3 style="color: #66c666">Program Pointer</h3>
+                    <form role="form" method="post" action="">
+                        <div class="form-group">
+                            <input type="email" class="form-control input-lg" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control input-lg" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-orange btn-block btn-lg">Submit</button><br/>
+                        <a class="btn-link" href="#">Forgot Password</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
