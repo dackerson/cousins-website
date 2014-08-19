dc2a64d4f0ff301bb47ecdf4c61d1f3274342b03
lucasboko <lucasboko@users.noreply.github.com>
2014-08-19 10:46:43 -0300

    Create admin-top.php

---
+++ admin-top.php
@@ -0,0 +1,4 @@
+<div class="navbar navbar-inverse navbar-fixed-top "  id="" style="" role="navigation">
+    <span class="navbar-brand" id="site-title">Cousin Website Admin</span>
+    <img class="side-survey-btn-inactive" id="side-survey-btn" src="img/rsz_admin-menu.png" alt=""/>
+</div>
