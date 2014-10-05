165abd7ee17caabedcab5eae1fb1a79f98e042e0
David Ackerson <david.ackerson2@gmail.com>
2014-10-05 00:04:31 -0300

    Adding Lucas' changes to the views

--- app/assets/javascripts/cousinwebsite.js
+++ app/assets/javascripts/cousinwebsite.js
@@ -61,12 +61,14 @@ $(document).ready(function() {
 
     $(document).on("click", "label.label-choice-active", function() {
         $(this).removeClass("active-label label-choice-active").addClass("label-choice");
-        $('#' + $(this).attr('for')).attr('checked', false);
+        // $('#' + $(this).attr('for')).attr('checked', 'checked');
+        $(this).children().attr("checked", false);
     });
 
-    $(document).on("click", ".label-choice", function() {
+    $(document).on("click", "label.label-choice", function() {
         $(this).addClass("active-label label-choice-active").removeClass("label-choice");
-        $('#' + $(this).attr('for')).attr('checked', true);
+        // $('#' + $(this).attr('for')).attr('checked', 'checked');
+        $(this).children().attr("checked", true);
     });
 
     $(document).on("click", ".label-choice-radio", function() {
