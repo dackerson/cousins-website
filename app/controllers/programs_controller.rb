ae7e52e34fe96122d9ba8f2317d4af9244187b73
David Ackerson <david.ackerson2@gmail.com>
2015-03-24 22:00:02 -0300

    Fixed contact fields not being added to new programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -137,6 +137,6 @@ class ProgramsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def program_params
-      params.require(:program).permit(:name, :desc, :website)
+      params.require(:program).permit(:name, :desc, :website, :contact_name, :contact_phone, :contact_email)
     end
 end
